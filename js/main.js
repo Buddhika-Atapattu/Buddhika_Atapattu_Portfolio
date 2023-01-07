78107fd78096293009b2fbd87352b5de7a0319a9
Buddhika Atapattu <88162177+Buddhika-Atapattu@users.noreply.github.com>
2023-01-08 03:42:26 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
 $(document).ready(function () {
     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    var scroll_icon = $("#arrow_to_up");
+
+    scroll_icon.click(function () {
+        $(window).scrollTop(0);
+    });
+
+    $(window).scroll(function () {
+        if ($(window).scrollTop() > 100) {
+            $("#arrow_to_up").css({ "display": "flex", "bottom": "0", "opacity": "1", "position": "fixed" });
+            // $("#arrow_to_up").addClass("animation-icon");
+        }
+        else {
+            $("#arrow_to_up").css({ "opacity": "0" });
+        }
+    });
 });
+
